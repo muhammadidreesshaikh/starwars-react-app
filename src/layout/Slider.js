0040cb5f7e9533f609906a325a2973f20ff51c1a
Anis Iqbal <anisiqbal484@gmail.com>
2020-04-05 22:23:22 +0500

    data coming from variables

--- src/layout/Slider.js
+++ src/layout/Slider.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../assets/css/slider.css';
 
+let heading = "Download StarWars.com's Exclusive Star Wars: The Rise of Skywalker Covers!";
+let subHeading = "The final film in the Skywalker saga is home on 4K Ultra HD and Blu-ray. Customize your copy today!";
+let link = "https://www.google.com";
 
 function Slider() {
     return(
@@ -11,11 +14,11 @@ function Slider() {
 
                         <div className="col-sm-4 col-md-4 col-lg-4">
                             <div className="head">
-                                <h2>Download StarWars.com's Exclusive Star Wars: The Rise of Skywalker Covers!</h2>
-                                <h6>The final film in the Skywalker saga is home on 4K Ultra HD and Blu-ray. Customize your copy today!</h6>
+                                <h2>{heading}</h2>
+                                <h6>{subHeading}</h6>
 
                                 <div className="btn">
-                                    <a href="#">GET THEM HERE</a> 
+                                    <a href={link}>GET THEM HERE</a> 
                                 </div>
                             </div>
                         </div>
