ec512dcb3be43c5e4a168341a49e9c18070cf7c2
Anis Iqbal <anisiqbal484@gmail.com>
2020-04-05 22:13:18 +0500

    footer init

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,7 +5,36 @@ function Footer(){
     return(
       <div>
           <div className="footer" >
-              <p>idreesiqbal104@gmail.com</p>
+            <div className="container-fluid">
+                <div className="row">
+                    
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+
+                        <h6>FOLLOW STAR WARS:</h6>
+
+                        <div className="icon">
+                            <i class="fab fa-facebook-f"></i> 
+                            <i class="fab fa-instagram"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-youtube"></i>   
+                        </div>
+
+                        <p>TM & © Lucasfilm Ltd. All Rights Reserved</p>
+
+                        <div className="btn2">
+                            <a href="#">Terms of Use</a>
+                            <a href="#"> Additional Content Information</a>
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">Terms of Use</a>
+                            <a href="#">Terms of Use</a>
+                            <a href="#">Terms of Use</a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
           </div>
       </div>  
     );
