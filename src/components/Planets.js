d7f47aca24f241df1404609d0bf831a12346af52
Anis Iqbal <anisiqbal484@gmail.com>
2020-04-05 23:16:11 +0500

    iterating the array of obejcts using loops

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/css/planets.css';
-import image from '../assets/img/p1.webp';
+
+import image0 from '../assets/img/p1.webp';
 import image1 from '../assets/img/p2.jpeg';
 import image2 from '../assets/img/p3.webp';
 import image3 from '../assets/img/p4.webp';
@@ -9,6 +10,29 @@ import image5 from '../assets/img/p6.webp';
 import image6 from '../assets/img/p7.jpeg';
 import image7 from '../assets/img/p8.webp';
 
+// array 
+let planetsData = [
+    {
+        image: 'https://images.wallpapersden.com/image/wxl-black-widow-in-avengers-endgame_64416.jpg',
+        heading: 'FIRST ORDER JET TROOPERS',
+        text: 'Specialized stormtroopers of the First Order, jet troopers soar into battle'
+    },
+    {
+        image: 'https://images.wallpapersden.com/image/wxl-black-widow-in-avengers-endgame_64416.jpg',
+        heading: 'SECOND ORDER JET TROOPERS',
+        text: 'Specialized stormtroopers of the First Order, jet troopers soar into battle'
+    },
+    {
+        image: 'https://images.wallpapersden.com/image/wxl-black-widow-in-avengers-endgame_64416.jpg',
+        heading: 'THIRD ORDER JET TROOPERS',
+        text: 'Specialized stormtroopers of the First Order, jet troopers soar into battle'
+    },
+    {
+        image: 'https://images.wallpapersden.com/image/wxl-black-widow-in-avengers-endgame_64416.jpg',
+        heading: 'FOURTH ORDER JET TROOPERS',
+        text: 'Specialized stormtroopers of the First Order, jet troopers soar into battle'
+    }
+]
 
 function Planets() {
     return(
@@ -22,109 +46,26 @@ function Planets() {
                             <div className="seprator"></div>
                         </div>
 
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image} />
-                                </div>
+                        {
+                            planetsData.map(
+                                (planet, i) => {
+                                    return(
+                                        <div className="col-sm-3 col-md-3 col-lg-3">
+                                            <div className="wrapper1">
+                                                <div className="img2">
+                                                    <img src={planet.image} />
+                                                </div>
 
-                                <div className= "head">
-                                    <h6>FIRST ORDER JET TROOPERS</h6>
-                                    <p>Specialized stormtroopers of the First Order, jet troopers soar into battle equipped with agile rocket packs.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image1} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>AP’LEK’S VIBRO-AX</h6>
-                                    <p>Once the property of a feared Mandalorian executioner, Ap’lek’s vibro-ax is now an emblem of the Knights of Ren.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image2} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>FIRST ORDER JET TROOPERS</h6>
-                                    <p>Specialized stormtroopers of the First Order, jet troopers soar into battle equipped with agile rocket packs.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image3} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>SITH ETERNAL</h6>
-                                    <p>On the lost world of Exegol, Sith cultists gather to venerate the ancient Sith Order and swear fealty to the current Sith Lord.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image4} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>FORCE HEALING</h6>
-                                    <p>Exploring the Jedi texts, Rey discovers descriptions of Jedi healing techniques, which she instinctively uses to aid a vexis.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image5} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>LANDO’S PROD PISTOL</h6>
-                                    <p>Supreme Leader Kylo Ren speeds into battle aboard a TIE whisper modified to his exacting specification.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image6} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>ADMIRAL GRISS</h6>
-                                    <p>A veteran Frantis Griss commands the Steadfast and its battlegroup and sits on the Supreme Council.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 col-md-3 col-lg-3">
-                            <div className="wrapper1">
-                                <div className="img2">
-                                    <img src= {image7} />
-                                </div>
-
-                                <div className= "head">
-                                    <h6>NAMBI GHIMA</h6>
-                                    <p>A young Aki-Aki girl, Nambi Ghima greets Rey at the Festival of Ancestors on Pasaana, offering a husk necklace as a gesture of welcome and inviting Rey to share her family history.</p>
-                                </div>
-                            </div>
-                        </div>
+                                                <div className= "head">
+                                                    <h6>{planet.heading}</h6>
+                                                    <p>{planet.text}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            )
+                        }
 
                     </div>
 
