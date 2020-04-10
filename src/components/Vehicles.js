a2f5008520206a8dd36ac04d8bb3029ba441ce91
anisiqbal <anisiqbal484@gmail.com>
2020-04-10 20:33:01 +0500

    vehicles api done

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,83 +1,84 @@
 import React from 'react';
 import '../assets/css/vehicles.css';
+
 import image2 from '../assets/img/f2.jpeg';
-import image3 from '../assets/img/f3.jpeg';
-import image4 from '../assets/img/f4.webp';
-import image5 from '../assets/img/f5.webp';
-import image6 from '../assets/img/f6.jpeg';
 
+import axios from 'axios';
 
-// arary
-let VehiclesData = [
-    {
-        heading: 'SOLO: A STAR WARS STORY',
-        text: 'Board the Millennium Falcon and journey to a galaxy far, far away in Solo: A Star Wars Story, an all-new adventure with the most beloved scoundrel in the galaxy. Through a series of daring escapades deep within a dark and dangerous criminal underworld, Han Solo meets his mighty future copilot Chewbacca and encounters the notorious gambler Lando Calrissian, in a journey that will set the course of one of the Star Wars saga’s most unlikely heroes.',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png'
-    },
-    {
-        heading: 'STAR WARS: THE LAST JEDI',
-        text: 'Rey has found the legendary Luke Skywalker, hoping to be trained in the ways of the Force. Meanwhile, the First Order seeks to destroy the remnants of the Resistance and rule the galaxy unopposed.',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png'
-    },
-    {
-        heading: 'ROGUE ONE: A STAR WARS STORY',
-        text: 'From Lucasfilm comes the first of the Star Wars standalone films, “Rogue One: A Star Wars Story,” an all-new epic adventure. In a time of conflict, a group of unlikely heroes band together on a mission to steal the plans to the Death Star, the Empire’s ultimate weapon of destruction. This key event in the Star Wars timeline brings together ordinary people who choose to do extraordinary things, and in doing so, become part of something greater than themselves.',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png'
-    },
-    {
-        heading: 'STAR WARS: RETURN OF THE JEDI',
-        text: 'Luke Skywalker leads a mission to rescue his friend Han Solo from the clutches of Jabba the Hutt, while the Emperor seeks to destroy the Rebellion once and for all with a second dreaded Death Star.',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png'
+class Vehicles extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            vehiclesData: []
+        };
     }
-]
 
+    componentDidMount() {
+        const url = "https://5e9044022810f4001648acfb.mockapi.io/api/v1/vehicles";
 
-function Vehicles() {
-    return(
-        <div>
-            <div className="vehicles">
-               <div className="container">
-                   <div className="row">
+        axios.get(url).then(responce => {
+            console.log(responce);
 
-                        <div className="col-sm-12 col-md-12 col-lg-12">
-                            <h3>Vehicles</h3>
-                            <div className="seprator"></div>
-                        </div>
+            // getting array from responce 
+            let vehicles = responce.data;
+            
+            // setting the planets state with api responce 
+            this.setState({
+                vehiclesData: vehicles
+            });
+        })
+    }
 
-                        {
-                            VehiclesData.map(
-                                (vehicle, i) => {
-                                    return(
-                                        <div className="col-sm-12 col-md-12 col-lg-12 px-0">
-                                            <div className="movie">
-                                                <div className="row">
-
-                                                    <div className="col-sm-7 col-md-7 col-lg-7">
-                                                        <div className="name">
-                                                            <h4> {vehicle.heading} </h4>
-                                                            <p>{vehicle.text}</p>
+    render() {
+        return(
+            <div>
+                <div className="vehicles">
+                   <div className="container">
+                       <div className="row">
+    
+                            <div className="col-sm-12 col-md-12 col-lg-12">
+                                <h3>Vehicles</h3>
+                                <div className="seprator"></div>
+                            </div>
+    
+                            {
+                                this.state.vehiclesData.map(
+                                    (vehicles, i) => {
+                                        return(
+                                            <div className="col-sm-12 col-md-12 col-lg-12 px-0" key={i}>
+                                                <div className="movie">
+                                                    <div className="row">
+    
+                                                        <div className="col-sm-7 col-md-7 col-lg-7">
+                                                            <div className="name">
+                                                                <h4> {vehicles.heading} </h4>
+                                                                <p>{vehicles.text}</p>
+                                                            </div>
                                                         </div>
-                                                    </div>
-
-                                                    <div className="col-sm-5 col-md-5 col-lg-5">
-                                                        <div className="img1">
-                                                            <img src={vehicle.image} />
+    
+                                                        <div className="col-sm-5 col-md-5 col-lg-5">
+                                                            <div className="img1">
+                                                                <img src={vehicles.avatar} />
+                                                            </div>
                                                         </div>
+    
                                                     </div>
-
                                                 </div>
                                             </div>
-                                        </div>
-                                    )
-                                }
-                            )
-                        }
-
+                                        )
+                                    }
+                                )
+                            }
+    
+                       </div>
                    </div>
-               </div>
+                </div>
             </div>
-        </div>
+    
+        );
+    }
 
-    );
 }
 export default Vehicles;
