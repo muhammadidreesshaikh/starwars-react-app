cc1c8403856cb8a42184ee151efe51566695e3e5
anisiqbal <anisiqbal484@gmail.com>
2020-04-10 17:01:48 +0500

    spaceship api done

--- src/components/Spaceships.js
+++ src/components/Spaceships.js
@@ -11,18 +11,18 @@ class Spaceships extends React.Component{
         super(props);
 
         this.state = {
-            spaceships: []
+            spaceshipsData: []
         };
     }
 
     componentDidMount() {
-        const url = "https://swapi.co/api/spaceships/";
+        const url = "https://5e9044022810f4001648acfb.mockapi.io/api/v1/spaceship";
 
         axios.get(url).then(responce => {
             console.log(responce);
 
             // getting array from responce 
-            let spaceships = responce.data.results;
+            let spaceships = responce.data;
             
             // setting the planets state with api responce 
             this.setState({
@@ -47,20 +47,20 @@ class Spaceships extends React.Component{
                                  this.state.spaceshipsData.map(
                                     (spaceship, i) => {
                                         return(
-                                            <div className="col-sm-12 col-md-12 col-lg-12 px-0">
+                                            <div className="col-sm-12 col-md-12 col-lg-12 px-0" key={i}>
                                                 <div className="movie">
                                                     <div className="row">
     
                                                         <div className="col-sm-7 col-md-7 col-lg-7">
                                                             <div className="name">
-                                                                <h4>{spaceship.name}</h4>
-                                                                <p>{spaceship.terrain}</p>
+                                                                <h4>{spaceship.title}</h4>
+                                                                <p>{spaceship.description}</p>
                                                             </div>
                                                         </div>
     
-                                                        <div className="col-sm-5 col-md-5 col-lg-5" key={i}>
+                                                        <div className="col-sm-5 col-md-5 col-lg-5">
                                                             <div className="img1">
-                                                                <img src={imageSpaceship} />
+                                                                <img src={spaceship.avatar} />
                                                             </div>
                                                         </div>
     
